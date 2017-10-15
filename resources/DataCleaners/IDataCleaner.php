b81ba1234bfef15f6332f4cd6056ef12953484bc
Kevin Zwarts <zwartsk@gmail.com>
2017-10-15 18:14:44 +0200

    initial

---
+++ resources/DataCleaners/IDataCleaner.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 10/15/2017
+ * Time: 6:13 PM
+ */
+
+interface IDataCleaner
+{
+    /**
+     * Clean a specified set of data from WP
+     *
+     * @return mixed
+     */
+    public function clean();
+
+}
